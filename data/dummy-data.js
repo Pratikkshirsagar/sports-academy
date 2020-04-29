13320ba9c99af30e2883a1105ebfeff86ff2f550
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-29 13:43:54 +0530

    dummy data added

---
+++ data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from '../models/category';
+
+export const CATEGORIES = [
+  new Category('c1', 'Karate', '#f5428d'),
+  new Category('c2', 'Tennis Court', '#f54242'),
+  new Category('c3', 'Squash Court', '#f5a442'),
+  new Category('c4', 'Badminton', '#f5d142'),
+  new Category('c5', 'Football', '#368dff'),
+  new Category('c6', 'Cricket', '#41d95d'),
+  new Category('c7', 'Skating', '#9eecff'),
+  new Category('c8', 'Box Cricket', '#b9ffb0'),
+  new Category('c9', 'Teakwondo', '#ffc7ff'),
+  new Category('c10', 'Golf', '#47fced'),
+];
