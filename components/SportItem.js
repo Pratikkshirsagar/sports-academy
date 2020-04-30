61f00edbc59b7a81cff1368af6bcb3d7f6ca1ced
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-30 13:41:27 +0530

    sport list item component added

---
+++ components/SportItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const SportItem = () => {
+  return (
+    <View>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SportItem;
