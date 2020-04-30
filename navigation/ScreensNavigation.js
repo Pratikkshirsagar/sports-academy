eb07ffeb46223d5bebf0baaedb5c93c3609869a7
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-01 00:11:45 +0530

    bot tab navigation added

--- navigation/ScreensNavigation.js
+++ navigation/ScreensNavigation.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategorySportScreen from '../screens/CategorySportScreen';
 import SportDetailScreen from '../screens/SportDetailScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import Colors from '../constants/Colors';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const SportsNavigator = createStackNavigator(
   {
@@ -28,9 +32,35 @@ const SportsNavigator = createStackNavigator(
   }
 );
 
+const SportsFavTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: SportsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-home" size={27} color={tabInfo.tintColor} />;
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-star" size={27} color={tabInfo.tintColor} />;
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
 const AppSwitchNavigater = createSwitchNavigator({
   WelcomeScreen: WelcomeScreen,
-  SportsNavigator: SportsNavigator,
+  SportsNavigator: SportsFavTabNavigator,
 });
 
 export default createAppContainer(AppSwitchNavigater);
