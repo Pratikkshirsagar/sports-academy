909a31b28ce8aa0027f92b7a5352db465bf0f977
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-06 23:23:24 +0530

    past and upcoming events screen added

---
+++ screens/UpcomingEventsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const UpcomingEvents = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Events</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default UpcomingEvents;
