05d922b7e04c18cd6dbadad4a96b0c500f16e9af
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-27 20:27:49 +0530

    booking cancelation done

--- screens/BookingScreen.js
+++ screens/BookingScreen.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, FlatList, Item, Button } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Item,
+  Button,
+  Alert,
+} from 'react-native';
 import { connect } from 'react-redux';
 
 import { firestore } from '../config/config';
@@ -8,6 +16,35 @@ import { Card } from 'react-native-shadow-cards';
 const BookingScreen = (props) => {
   const [bookingList, setBookingList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const buttonPress = (id) => {
+    Alert.alert('Cancel Booking', 'Are you sure want to cancle booking', [
+      {
+        text: 'No',
+        onPress: () => {
+          console.log('No Press');
+        },
+      },
+      {
+        text: 'Yes',
+        onPress: async () => {
+          try {
+            const bookingRef = firestore.collection('booking');
+            let snapshot = await bookingRef.where('bookingId', '==', id).get();
+            if (snapshot.empty) {
+              alert('Booking not found');
+            }
+            let refDoc;
+            snapshot.forEach((doc) => {
+              doc.ref.delete();
+            });
+          } catch (err) {
+            console.log(err);
+          }
+        },
+      },
+    ]);
+  };
 
   const getBookingHistory = async () => {
     setIsLoading(true);
@@ -54,7 +91,7 @@ const BookingScreen = (props) => {
             <Button
               style={{ fontFamily: 'open-sans-bold', fontSize: 20 }}
               onPress={() => {
-                console.log('Work');
+                buttonPress(item.bookingId);
               }}
               title="Cancel Booking"
               color="#ff6f00"
